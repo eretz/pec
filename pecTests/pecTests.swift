64f9a67c27072cc6f98b8f1832f260d0c4a193b9
emmanuel chemla <emmanuel@f261.gestionova.fr>
2016-06-15 09:56:47 +0200

    Initial Commit

---
+++ pecTests/pecTests.swift
@@ -0,0 +1,36 @@
+//
+//  pecTests.swift
+//  pecTests
+//
+//  Created by emmanuel chemla on 15/06/2016.
+//  Copyright © 2016 emmanuel chemla. All rights reserved.
+//
+
+import XCTest
+@testable import pec
+
+class pecTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
